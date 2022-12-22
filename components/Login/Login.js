6e777dab3f655726077fa8a80f82c938d84d3d46
julie yudowitch <julieyudo@gmail.com>
2022-12-22 10:57:37 -0500

    styles

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -4,16 +4,30 @@ import { FcGoogle } from "react-icons/fc";
 import { signIn } from "next-auth/react";
 import PublicFeed from "../PublicFeed/PublicFeed";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 export default function Login() {
   return (
     <>
       <Header />
+      <h2 className={styles.loginPrompt}>
+        Hello and welcome to my gardening blog!
+      </h2>
+      <h3 className={styles.loginPrompt}>
+        This is a place to celebrate all things plants.
+      </h3>
+      <p className={styles.loginPrompt}>
+        Please bear with me as this site is still under construction
+      </p>
+      <h3 className={styles.loginPrompt}>
+        Login to post your own garden related content
+      </h3>
       <div className={styles.login} onClick={() => signIn("google")}>
         <FcGoogle className={styles.glogo} />
         <p className={styles.prompt}>Sign in with Google</p>
       </div>
-      <PublicFeed />
+      <PublicFeed className={styles.publicFeed} />
+      <Footer />
     </>
   );
 }
--- components/PublicFeed/PublicFeed.js
+++ components/PublicFeed/PublicFeed.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import styles from './PublicFeed.module.css'
 import React, { useEffect, useState } from "react";
 import { onSnapshot, collection, query, orderBy } from "firebase/firestore";
