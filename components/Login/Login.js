9470fa4b1679846fced1a6698216768787be8614
julie yudowitch <julieyudo@gmail.com>
2022-12-22 01:09:16 -0500

    styled login page

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import styles from './Login.module.css'
+import React from "react";
+import styles from "./Login.module.css";
 import { FcGoogle } from "react-icons/fc";
 import { signIn } from "next-auth/react";
 
 export default function Login() {
   return (
-    <>     
-        <div onClick={() => signIn("google")}>
-          <FcGoogle/>
-          Sign in with Google
-        </div>      
+    <>
+      <div className={styles.login} onClick={() => signIn("google")}>
+        <FcGoogle className={styles.glogo} />
+        <p className={styles.prompt}>Sign in with Google</p>
+      </div>
     </>
   );
 }
