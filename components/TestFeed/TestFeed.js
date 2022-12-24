f9c45ae5fb178f5716075f5e34fcce7c062cdff6
julie yudowitch <julieyudo@gmail.com>
2022-12-24 07:50:08 -0500

    styles

--- components/TestFeed/TestFeed.js
+++ components/TestFeed/TestFeed.js
@@ -16,7 +16,7 @@ import React from 'react'
 export default function TestFeed() {
 
 const first = query(collection(db, "posts"), orderBy("asc"), limit(10));
-const documentSnapshots = await getDocs(first);
+
 
   return (
     <div>TestFeed</div>
